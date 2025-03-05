ddaafb77352000081aaafda885317fd64afec04b
Eden-Ismah-Moshe <eden12012@gmail.com>
2025-03-06 00:37:02 +0200

    Add recipe page

--- src/server.js
+++ src/server.js
@@ -2,8 +2,22 @@ const express = require("express");
 const axios = require("axios");
 const cors = require("cors");
 require("dotenv").config();
+const { Pool } = require("pg");
 
-const { GEMINI_API_KEY } = process.env;
+const { GEMINI_API_KEY, PG_USER, PG_PASSWORD, PG_HOST, PG_PORT, PG_DB_NAME } =
+  process.env;
+
+const pool = new Pool({
+  user: PG_USER,
+  password: PG_PASSWORD,
+  host: PG_HOST,
+  port: PG_PORT,
+  database: PG_DB_NAME,
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
 
 const GEMINI_API_URL = `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${GEMINI_API_KEY}`;
 
@@ -40,6 +54,26 @@ app.post("/generate-recipe", async (req, res) => {
       error.response ? error.response.data : error.message
     );
     throw error;
+  }
+});
+
+app.get("/recipe/:id", async (req, res) => {
+  const { id } = req.params;
+  console.log("ID: ", id);
+  try {
+    pool.query(
+      "SELECT * FROM recipes WHERE name = $1",
+      [id],
+      (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).json(results.rows[0]);
+      }
+    );
+  } catch (error) {
+    console.error("Error: ", error);
+    res.status(500).json({ error: error.message });
   }
 });
 
